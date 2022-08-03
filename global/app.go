8fd53ca853f79969e914cb8d2b3487b1f7d11e21
Liuyang <2391461132@qq.com>
2022-08-03 23:19:49 +0800

    初始化Redis

--- global/app.go
+++ global/app.go
@@ -3,6 +3,7 @@ package global
 /*用来存放一些项目启动时的变量*/
 import (
 	"gin-demo/config"
+	"github.com/go-redis/redis/v8"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
@@ -14,6 +15,7 @@ type Application struct {
 	Config      config.Configuration
 	Log         *zap.Logger
 	DB          *gorm.DB
+	Redis       *redis.Client
 }
 
 var App = new(Application)
