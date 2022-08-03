a3d032becff3e7159eb31e06d2de128244f82a98
Liuyang <2391461132@qq.com>
2022-08-03 11:23:50 +0800

    编写token颁发逻辑&实现登录接口

--- app/common/request/user.go
+++ app/common/request/user.go
@@ -1,7 +1,14 @@
 package request
 
+// Register 验证器结构体
 type Register struct {
 	Name     string `form:"name" json:"name" binding:"required" `
+	Mobile   string `form:"mobile" json:"mobile" binding:"required,mobile"`
+	Password string `form:"password" json:"password" binding:"required"`
+}
+
+// Login 验证器结构体
+type Login struct {
 	Mobile   string `form:"mobile" json:"mobile" binding:"required,mobile"`
 	Password string `form:"password" json:"password" binding:"required"`
 }
@@ -10,6 +17,14 @@ type Register struct {
 func (register Register) GetMessages() ValidatorMessages {
 	return ValidatorMessages{
 		"name.required":     "用户名不能为空",
+		"mobile.required":   "手机号码不能为空",
+		"mobile.mobile":     "手机号码格式不正确",
+		"password.required": "用户密码不能为空",
+	}
+}
+
+func (login Login) GetMessages() ValidatorMessages {
+	return ValidatorMessages{
 		"mobile.required":   "手机号码不能为空",
 		"mobile.mobile":     "手机号码格式不正确",
 		"password.required": "用户密码不能为空",
---
+++ app/controllers/app/auth.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"gin-demo/app/common/request"
+	"gin-demo/app/common/response"
+	"gin-demo/app/services"
+	"github.com/gin-gonic/gin"
+)
+
+func Login(c *gin.Context) {
+	var form request.Login
+	if err := c.ShouldBindJSON(&form); err != nil {
+		response.ValidateFail(c, request.GetErrorMsg(form, err))
+		return
+	}
+
+	if err, user := services.UserService.Login(form); err != nil {
+		response.BusinessFail(c, err.Error())
+	} else {
+		tokenData, err, _ := services.JwtService.CreateToken(services.AppGuardName, user)
+		if err != nil {
+			response.BusinessFail(c, err.Error())
+			return
+		}
+		response.Success(c, tokenData)
+	}
+}
